968321793ba1585e9b2eb482660d351dae8ab3d2
mayank <mahakjangid05@gmail.com>
2026-09-17 12:48:40 +0530

    pdf qa tool - initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "PDF QA Tool Running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
